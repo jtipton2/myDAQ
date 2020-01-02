ce5a96337c5fbc84495e514dcd46f11bb5c70dd2
Joseph Tipton <57552601+jtipton2@users.noreply.github.com>
2020-01-02 16:06:53 -0600

    Record AI on 2 simultaneous channels
    This program will use Hardware Timed Finite Sampling to collect data on 2, differential, AI channels simultaneously.

--- myDAQ_1Chan.m
+++ myDAQ_1Chan.m
@@ -1,41 +1,14 @@
 %% MATLAB myDAQ Data Acquisition File
-% Created by Garrett Wood (8/5/2017)
-% Modified by Joseph B. Tipton, Jr. (2/7/2018)
+% CREATED BY:   Garrett Wood (8/5/2017)
+% MODIFIED BY:  Joseph B. Tipton, Jr. (2/22/2018)
+% DESCRIPTION:  This program will use Hardware Timed Finite Sampling
+%               to collect data on 2, differential, AI channels
+%               simultaneously.
 
 clc
 clear all
 
 %% Graphical User Interface
-str = {'Hardware Timed Continuous Sampling','Software Timed Continuous Sampling','Hardware Timed Finite Sampling','Software Timed Finite Sampling'};
-[s,v] = listdlg('PromptString','Select one mode of data collection:',...
-                'SelectionMode','single',...
-                'ListString',str,...
-                'ListSize',[300 160]);
-            
-if v == 0
-    error('You must select a DAQ mode.')
-end
-
-switch s
-    case 1
-        %Hardware Timed Continuous Sampling
-        prompt = {'Enter either 2 or 10 for voltage range:','Enter sampling rate (Hz):'};
-        dlg_title = 'Hardware Timed Continuous Sampling';
-        num_lines = 1;
-        defaultans = {'2','1000'};
-        answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
-        if str2num(answer{1,:})~=2 && str2num(answer{1,:})~=10
-            error('Valid voltage ranges for NI myDAQ are +- 2V and +- 10V')
-        end
-        VoltageRange = [-str2num(answer{1,:}), str2num(answer{1,:})];
-        Rate = str2num(answer{2,:});
-        
-        Data = HWT_Continuous( VoltageRange, Rate );
-    case 2
-        %Software Timed Continuous Sampling
-        msgbox( 'This feature is still in development.')
-    case 3
-        %Hardware Timed Finite Sampling
         prompt = {'Enter either 2 or 10 for voltage range:','Enter sampling rate (Hz):','Enter test duration (sec):'};
         dlg_title = 'Hardware Timed Finite Sampling';
         num_lines = 1;
@@ -47,75 +20,19 @@ switch s
         VoltageRange = [-str2num(answer{1,:}), str2num(answer{1,:})];
         Rate = str2num(answer{2,:});
         Duration = str2num(answer{3,:});
-        
+
+
+%% Collect Data from the NI myDAQ using the MATLAB Drivers
         Data = HWT_Finite( VoltageRange, Rate , Duration );
-    case 4
-        %Software Timed Finite Sampling
-        prompt = {'Enter either 2 or 10 for voltage range:','Enter pause between samples (sec):','Enter number of samples:'};
-        dlg_title = 'Software Timed Finite Sampling';
-        num_lines = 1;
-        defaultans = {'2','0.5','4'};
-        answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
-        if str2num(answer{1,:})~=2 && str2num(answer{1,:})~=10
-            error('Valid voltage ranges for NI myDAQ are +- 2V and +- 10V')
-        end
-        VoltageRange = [-str2num(answer{1,:}), str2num(answer{1,:})];
-        Pause = str2num(answer{2,:});
-        NSamples = str2num(answer{3,:});
-        
-        Data = SWT_Finite( VoltageRange, Pause , NSamples );
-    otherwise
-        error('You must select a DAQ mode.')
-end
 
-%% Data Collection Subroutines
-function [ Data ] = HWT_Continuous( range, rate )
-    
-    Times = [];
-    SessionData = [];
-    
-    %%  Create a data acquisition session
-    
-    daqSession_0 = daq.createSession( 'ni' );
 
-    %%  Add channels specified by subsystem type and device
-    
-    ch1 = daqSession_0.addAnalogInputChannel( 'myDAQ1' , 'ai0' , 'Voltage' );
-    ch1.Range = range;
-    
-    %%  Configure properties
-    
-    daqSession_0.Rate = rate;
-    daqSession_0.IsContinuous = 1;
-    daqSession_0.addlistener( 'DataAvailable' , @HWT_C_Data );
-    
-    %% Data Acquisition Procedure
-    
-    function HWT_C_Data( ~ , event )
-        
-        Times = [ Times ; event.TimeStamps ];
-        SessionData = [ SessionData ; event.Data ];
-        plot( Times , SessionData )
-        
-    end
+%% CUSTOM ANALYSIS
+    % Once data collection is complete, you will have an array called
+    % "Data".  You can add your custom code here to analyze the data as
+    % needed.
 
-    %%  Begin Data Acquisition Session
-    
-    daqSession_0.startBackground();
-    fprintf( 'Click on figure to end Data Acquisition.\n' )
-    waitforbuttonpress;
-    fprintf( 'Data Acquisition ended.\n' )
-    Data = [ Times , SessionData ];
-    daqSession_0.stop()
-    
-    %%  Disconnect from the device
-    
-    daqSession_0.release();
-    delete( daqSession_0 );
-    clear daqSession_0;
-    
-end 
 
+%% Data Collection Subroutines ============================================
 
 function [ Data ] = HWT_Finite( range, rate , dur )
 
@@ -127,6 +44,9 @@ function [ Data ] = HWT_Finite( range, rate , dur )
     
     ch1 = daqSession_0.addAnalogInputChannel( 'myDAQ1' , 'ai0' , 'Voltage' );
     ch1.Range = range;
+    
+    ch2 = daqSession_0.addAnalogInputChannel( 'myDAQ1' , 'ai1' , 'Voltage' );
+    ch2.Range = range;
     
     %%  Configure properties
     
@@ -145,46 +65,3 @@ function [ Data ] = HWT_Finite( range, rate , dur )
     clear daqSession_0;
     
 end 
-
-
-function [ Data ] = SWT_Finite( range, delay , N )
-
-    %% Test Code for Timer Function
-    Data = TestTimer( range, N , delay );
-
-    function [ Data ] = TestTimer( range, N , delay )
-
-        %% Timer Setup
-
-        Data = [];
-        AcquisitionTimer = timer;
-        AcquisitionTimer.TimerFcn = @( ~ , thisEvent ) TimedData(range);
-        AcquisitionTimer.TasksToExecute = N;
-        AcquisitionTimer.Period = delay;
-        AcquisitionTimer.ExecutionMode = 'fixedrate';
-        start( AcquisitionTimer )
-
-        %% Kill Timer
-
-        pause( delay*N ) 
-        delete( AcquisitionTimer )
-
-        function TimedData(range)
-
-            %% Acquisition Setup
-
-            daqSession_0 = daq.createSession( 'ni' );
-            ch1 = daqSession_0.addAnalogInputChannel( 'myDAQ1' , 'ai0' , 'Voltage' );
-            ch1.Range = range;
-                        
-            Data = [ Data ; clock , daqSession_0.inputSingleScan; ];
-
-            daqSession_0.release();
-            delete( daqSession_0 );
-            clear daqSession_0;  
-
-        end
-
-    end
-
-end
